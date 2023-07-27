ea71b6aab7c84d708f760c6683e378e2140530a6
SeaWorld <robert.klitsch97@gmail.com>
2023-07-27 16:17:10 -0700

    Change to reversable model type

--- src/HasKpi.php
+++ src/HasKpi.php
@@ -5,12 +5,13 @@ namespace Finller\Kpi;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 use Finller\Kpi\Enums\KpiInterval;
+use Illuminate\Support\Facades\DB;
 
 trait HasKpi
 {
     public static function getKpiNamespace(): string
     {
-        return Str::lower(Str::plural(class_basename(static::class)));
+        return Str::snake(class_basename(static::class));
     }
 
     public static function kpi(string $key = 'count'): KpiBuilder
@@ -37,7 +38,7 @@ trait HasKpi
         ?Carbon $start = null,
         ?Carbon $end = null,
         ?string $key = 'count',
-    ): array {
+    ): bool {
         $kpiModel = config('kpi.kpi_model');
         /** @var ?Carbon */
         $start = $start ?? Carbon::parse(static::min($column));
@@ -57,7 +58,9 @@ trait HasKpi
             ])
             ->toArray();
 
-        foreach ($fillDates as $k => $date) {
+        DB::beginTransaction();
+
+        foreach ($fillDates as $date) {
             if (isset($date['id'])) continue;
 
             $kpi = new $kpiModel();
@@ -71,12 +74,16 @@ trait HasKpi
                 $date
             ));
 
-            $kpi->save();
-
-            $fillDates[$k] = $kpi->toArray();
+            if (!$kpi->save()) {
+                DB::rollBack();
+                /** @return bool */
+                return false;
+            }
         }
-        /** @return array */
-        return $fillDates;
+
+        DB::commit();
+        /** @return bool */
+        return true;
     }
 
     public static function backfillKpiCount(
@@ -85,7 +92,7 @@ trait HasKpi
         ?Carbon $start = null,
         ?Carbon $end = null,
         ?string $key = 'count'
-    ): array {
+    ): bool {
         return static::backfillKpi(
             function ($model, $start, $end, $key, $date) use ($column) {
                 $count = $model->whereBetween($column, [
